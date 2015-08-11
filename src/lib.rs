814afaa816ba2b8cfbc8c2d00c6b1d8d86bef921
Michael Eden <themichaeleden@gmail.com>
2015-08-11 15:36:20 -0400

    Added Number and Integer traits, bit ops limited to i32/64, added field struct to not need to repeat field param
    Everything is much better.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,26 @@
 use std::collections::BTreeMap;
-use std::fmt::{Arguments, Display};
 
-type Object<'a> = BTreeMap<&'a str, SerJson<'a>>;
+pub type Object<'a> = BTreeMap<&'a str, Bson<'a>>;
 
-#[derive(Clone)]
-enum SerJson<'a> {
-    Map(Object<'a>),
-    RawJson(Arguments<'a>),
-    I32(i32),
-    U32(u32),
-    F64(f64),
+/// Possible BSON value types.
+#[derive(Debug, Clone)]
+pub enum Bson<'a> {
+    FloatingPoint(f64),
     Str(&'a str),
-    Value(&'a Display),
+//    Array(Array),
+//    Document(Document),
+    Boolean(bool),
+    Null,
+//    RegExp(String, String),
+//    JavaScriptCode(String),
+//    JavaScriptCodeWithScope(String, Document),
+    I32(i32),
+    I64(i64),
+    Object(Object<'a>),
+//    TimeStamp(i64),
+//    Binary(BinarySubtype, Vec<u8>),
+//    ObjectId(oid::ObjectId),
+//    UtcDatetime(DateTime<UTC>),
 }
 
 trait Upsert<'a> {
@@ -21,15 +30,59 @@ trait Upsert<'a> {
 impl<'a> Upsert<'a> for Object<'a> {
     #[inline]
     fn object(&mut self, field: &'a str) -> &mut Self {
-        match self.entry(field).or_insert(SerJson::Map(Object::new())) {
-            &mut SerJson::Map(ref mut m) => m,
+        match self.entry(field).or_insert(Bson::Object(Object::new())) {
+            &mut Bson::Object(ref mut o) => o,
             _ => unreachable!(),
         }
     }
 }
 
+pub trait Integer<'a> {
+    fn to_bson_int(self) -> Bson<'a>;
+}
+
+impl<'a> Integer<'a> for i32 {
+    #[inline]
+    fn to_bson_int(self) -> Bson<'a> {
+        Bson::I32(self)
+    }
+}
+
+impl<'a> Integer<'a> for i64 {
+    #[inline]
+    fn to_bson_int(self) -> Bson<'a> {
+        Bson::I64(self)
+    }
+}
+
+impl<'a> Integer<'a> for u32 {
+    #[inline]
+    fn to_bson_int(self) -> Bson<'a> {
+        Bson::I64(self as i64)
+    }
+}
+
+pub trait Number<'a> {
+    fn to_bson_num(self) -> Bson<'a>;
+}
+
+
+impl<'a> Number<'a> for f64 {
+    #[inline]
+    fn to_bson_num(self) -> Bson<'a> {
+        Bson::FloatingPoint(self)
+    }
+}
+
+impl<'a, I: Integer<'a>> Number<'a> for I {
+    #[inline]
+    fn to_bson_num(self) -> Bson<'a> {
+        self.to_bson_int()
+    }
+}
+
 mod update;
-pub use update::Update;
+pub use update::{Update, UpdateField};
 
 #[test]
 fn it_works() {
--- src/update.rs
+++ src/update.rs
@@ -1,67 +1,80 @@
-use super::SerJson::*;
-use super::SerJson;
-use super::Object;
-use super::Upsert; 
+use super::{Object, Upsert, Number, Integer, Bson};
 
 #[derive(Clone)]
-pub struct Update<'a>(Object<'a>);
-
-/* TODO: Should *all* bit operations be with u32? */
+pub struct Update<'a> {
+    map: Object<'a>,
+}
 
 impl<'a> Update<'a> {
     pub fn new() -> Self {
-        Update(Object::new())
+        Update {
+            map: Object::new(),
+        }
     }
 
-    pub fn increment(self, field: &'a str, amount: f64) -> Self {
-        self.add_modifier(INCREMENT, field, F64(amount))
+    pub fn field(&'a mut self, field: &'a str) -> UpdateField {
+        UpdateField {
+            object: self,
+            field:  field,
+        }
     }
+}
 
-    #[inline]
-    pub fn inc(self, field: &'a str, amount: f64) -> Self {
-        self.increment(field, amount)
-    }
+pub struct UpdateField<'a> {
+    object: &'a mut Update<'a>,
+    field:  &'a str,
+}
 
-    pub fn multiply(self, field: &'a str, amount: f64) -> Self {
-        self.add_modifier(MULTIPLY, field, F64(amount))
-    }
-
-    #[inline]
-    pub fn mul(self, field: &'a str, amount: f64) -> Self {
-        self.multiply(field, amount)
-    }
-
-    pub fn and(self, field: &'a str, bits: u32) -> Self {
-        self.bit(AND, field, bits)
-    }
-
-    pub fn or(self, field: &'a str, bits: u32) -> Self {
-        self.bit(OR, field, bits)
-    }
-
-    pub fn xor(self, field: &'a str, bits: u32) -> Self {
-        self.bit(XOR, field, bits)
-    }
-
-    pub fn rename(self, field: &'a str, name: &'a str) -> Self {
-        self.add_modifier(RENAME, field, Str(name))
+impl<'a> UpdateField<'a> {
+    pub fn increment<N: Number<'a>>(self, amount: N) -> Self {
+        self.add_modifier(INCREMENT, amount.to_bson_num())
     }
 
     #[inline]
-    fn add_modifier(mut self, category: &'static str, field: &'a str, value: SerJson<'a>) -> Self {
+    pub fn inc<N: Number<'a>>(self, amount: N) -> Self {
+        self.increment(amount)
+    }
+
+    pub fn multiply<N: Number<'a>>(self, amount: N) -> Self {
+        self.add_modifier(MULTIPLY, amount.to_bson_num())
+    }
+
+    #[inline]
+    pub fn mul<N: Number<'a>>(self, amount: N) -> Self {
+        self.multiply(amount)
+    }
+
+    pub fn and<I: Integer<'a>>(self, bits: I) -> Self {
+        self.bit(AND, bits.to_bson_int())
+    }
+
+    pub fn or<I: Integer<'a>>(self, bits: I) -> Self {
+        self.bit(OR, bits.to_bson_int())
+    }
+
+    pub fn xor<I: Integer<'a>>(self, bits: I) -> Self {
+        self.bit(XOR, bits.to_bson_int())
+    }
+
+    pub fn rename(self, name: &'a str) -> Self {
+        self.add_modifier(RENAME, Bson::Str(name))
+    }
+
+    #[inline]
+    fn add_modifier(mut self, category: &'static str, value: Bson<'a>) -> Self {
         {
-            let group = self.0.object(category);
-            group.insert(field, value);
+            let group = self.object.map.object(category);
+            group.insert(self.field, value);
         }
         self
     }
 
     #[inline]
-    fn bit(mut self, op: &'static str, field: &'a str, bits: u32) -> Self {
+    fn bit(mut self, op: &'static str, bits: Bson<'a>) -> Self {
         {
-	        let bits_update = self.0.object(BIT);
-	        let field_ops = bits_update.object(field);
-	        field_ops.insert(op, U32(bits));
+	        let bits_update = self.object.map.object(BIT);
+	        let field_ops = bits_update.object(self.field);
+	        field_ops.insert(op, bits);
         }
         self
     }
